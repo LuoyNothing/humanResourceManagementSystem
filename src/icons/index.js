58aa9fa12e5ca29c666b236cc2592cead311bcac
luoyi66 <muxuejiaozyl@163.com>
2024-04-05 16:35:47 +0800

    加静态资源

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import SvgIcon from "@/components/SvgIcon"; // svg component
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
-Vue.component("SvgIcon", SvgIcon);
+Vue.component('svg-icon', SvgIcon)
 
-// require.context(文件夹地址，是否扫码子目录， 正则表达式-扫描什么文件) 用于扫码文件夹中的文件
-const req = require.context("./svg", false, /\.svg$/);
-// reg返回的是一个函数，req.keys()可以查看扫描出来的文件
-const requireAll = (requireContext) =>
-  requireContext.keys().map(requireContext);
-// requireAll 表示对每一个扫描出来的文件都应用到项目中
-requireAll(req);
+// 下面三行代码的任务是 把 同级目录的 svg目录下的.svg图片引入到项目中来
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+
+// 相当于把svg下的所有的svg图片打包到了项目中
+// 如果想用svg图片 就在svg目录下去寻找就可以了
